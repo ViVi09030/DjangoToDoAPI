f62a76726dd25d07cf44b3d258e29a598cae0744
ViVi09030 <vivianarango07@gmail.com>
2026-03-06 16:56:21 -0500

    feat(api): support listing and creating todos

--- api/views.py
+++ api/views.py
@@ -2,9 +2,12 @@ from rest_framework import generics
 from .serializers import ToDoSerializer
 from todo.models import ToDo
 
-class ToDoList(generics.ListAPIView):
+class ToDoListCreate(generics.ListCreateAPIView):
     serializer_class = ToDoSerializer
 
     def get_queryset(self):
         user = self.request.user
         return ToDo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
