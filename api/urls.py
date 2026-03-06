ae833256a14361fed51ba8d1a82fe0eccb2fe536
ViVi09030 <vivianarango07@gmail.com>
2026-03-06 17:17:42 -0500

    feat(auth): add signup and login API routes

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('todos/', views.ToDoListCreate.as_view(), name='todo_list'),
     path('todos/<int:pk>', views.ToDoRetrieveUpdateDestroy.as_view(), name='todo_rud'),
     path('todos/<int:pk>/complete', views.ToDoToggleComplete.as_view(), name='todo_complete'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
